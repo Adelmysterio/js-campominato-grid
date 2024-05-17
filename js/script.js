afb6c28e61721bd654c2475f5d2ac6d3112adcca
Adelmysterio <storliniadelmo@gmail.com>
2024-05-17 15:18:30 +0200

    aggiunta funzionalita' tasto play

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 const gridElement = document.getElementById('grid');
-
+const buttonElement = document.querySelector('button');
 
 for ( i=0 ; i < 100; i++) {
     const articleElement = document.createElement('article');
     gridElement.appendChild(articleElement);
+    articleElement.classList.add('big')
     const spanElement = document.createElement('span')
     articleElement.appendChild(spanElement);
     spanElement.append(i + 1)
 
     articleElement.addEventListener('click', function() {
         articleElement.classList.add('active')
+        console.log(spanElement.innerText)
+    })
+
+    buttonElement.addEventListener('click', function() {
+        articleElement.classList.remove('active')
         console.log(spanElement.innerText)
     })
 };
